c7d88377b0c3fe3aee4157f08011a529786e2fdf
Abdelrahman Gamal <108704227+AbdelrahmanGamal02@users.noreply.github.com>
2023-11-04 17:44:51 +0200

    File_Config.h

--- File_Config.h
+++ File_Config.h
@@ -1,19 +1,20 @@
 #ifndef FILE_H
 #define  FILE_H
 
+/************* Macros Definition *****************/
 #define Value_Of_End_File  EOF
 #define File_Closed_Successfully  0
+/*************************************************/
 
-
-
+/************** Helper Definition  ***************/
 FILE *Source_File = NULL;
 FILE *Destination_File = NULL;
-
 
 char Source_file_Path[100];
 char Destination_file_Path[100];
 
 char check_character = '\0';
 int Check_Close_File = 0;
+/*************************************************/
 
 #endif
